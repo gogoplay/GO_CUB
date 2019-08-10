ad920d8f62f1ec6fa76fb94c20196cf088efc944
gogoplay <gogoplaytim@gmail.com>
2019-08-10 23:19:11 +0800

    加入專案檔案。

---
+++ MvcParentDirectory/Controllers/ParentDirectoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcParentDirectory.Models;
+
+namespace MvcParentDirectory.Controllers
+{
+    public class ParentDirectoriesController : Controller
+    {
+        private ParentDirectoryDBContext db = new ParentDirectoryDBContext();
+
+        // GET: ParentDirectories
+        public ActionResult Index()
+        {
+            return View(db.ParentDirectories.ToList());
+        }
+
+        // GET: ParentDirectories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParentDirectory parentDirectory = db.ParentDirectories.Find(id);
+            if (parentDirectory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parentDirectory);
+        }
+
+        // GET: ParentDirectories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParentDirectories/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Address,Email,PhoneNum,CreateDate")] ParentDirectory parentDirectory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ParentDirectories.Add(parentDirectory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(parentDirectory);
+        }
+
+        // GET: ParentDirectories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParentDirectory parentDirectory = db.ParentDirectories.Find(id);
+            if (parentDirectory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parentDirectory);
+        }
+
+        // POST: ParentDirectories/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Address,Email,PhoneNum,CreateDate")] ParentDirectory parentDirectory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(parentDirectory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(parentDirectory);
+        }
+
+        // GET: ParentDirectories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParentDirectory parentDirectory = db.ParentDirectories.Find(id);
+            if (parentDirectory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parentDirectory);
+        }
+
+        // POST: ParentDirectories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ParentDirectory parentDirectory = db.ParentDirectories.Find(id);
+            db.ParentDirectories.Remove(parentDirectory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcParentDirectory/Models/ParentDirectory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcParentDirectory.Models
+{
+    public class ParentDirectory
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNum { get; set; }
+        [Display(Name = "Create Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime CreateDate { get; set; }
+    }
+
+    public class ParentDirectoryDBContext : DbContext
+    {
+        public DbSet<ParentDirectory> ParentDirectories { get; set; }
+    }
+}
